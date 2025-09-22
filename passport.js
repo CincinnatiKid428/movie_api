777bfb6294760e7266a4bf3344588888b2480d5c
CincinnatiKid428 <pweaver.psu03@gmail.com>
2025-09-21 21:02:00 -0400

    Added JSDoc & documentation
    - Updated code to add JSDoc tags in block comments
    - Added jsdoc.json with JSDoc configurations
    - Added /docs folder with documentation generated from JSDoc

--- passport.js
+++ passport.js
@@ -1,7 +1,11 @@
+//File: /passport.js - Holds authentication logic for HTTP Basic Auth and subsequent requests using JWT.
+
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const Models = require ('./models.js');
+const Models = require('./models.js');
 const passportJWT = require('passport-jwt');
+
+const DEBUG_LOG = false; //controls logging
 
 let Users = Models.User;
 let JWTStrategy = passportJWT.Strategy;
@@ -11,45 +15,46 @@ let ExtractJWT = passportJWT.ExtractJwt; //Used to pull JWT from request header
 passport.use(new LocalStrategy(
     {
         usernameField: 'Username',
-        passwordField: 'Password' 
+        passwordField: 'Password'
     },
-    async(username, password, callback) => {
-        console.log(`Authenticating username/passowrd : ${username} ${password}`);
-        await Users.findOne({Username:username})
-        .then((user) => {
-            //Valid username check
-            if(!user){
-                console.log('Incorrect username ['+username+'] no user found.');
-                return callback(null, false, {message: 'Incorrect username/password.'});
-            }
-            //Valid password check
-            if(!user.validatePassword(password)){
-                console.log('Incorrect password');
-                return callback(null, false, {message:'Incorrect password.'});
-            }
+    async (username, password, callback) => {
+        DEBUG_LOG && console.log(`Authenticating username/passowrd : ${username} ${password}`);
+        await Users.findOne({ Username: username })
+            .then((user) => {
+                //Valid username check
+                if (!user) {
+                    console.error('Incorrect username [' + username + '] no user found.');
+                    return callback(null, false, { message: 'Incorrect username/password.' });
+                }
+                //Valid password check
+                if (!user.validatePassword(password)) {
+                    console.error('Incorrect password');
+                    return callback(null, false, { message: 'Incorrect password.' });
+                }
 
-            console.log('Found user" '+user);
-            return callback(null, user);
-        })
-        .catch((err) => {
-            if(err) {
-                console.error(err);
-                return callback(err);
-            }
-        })
+                DEBUG_LOG && console.log('Found user" ' + user);
+                return callback(null, user);
+            })
+            .catch((err) => {
+                if (err) {
+                    console.error(err);
+                    return callback(err);
+                }
+            })
     }
 ));
 
 //JWT-based authentication (subsequent requests after login authentication)
 passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'your_jwt_secret'},
+    secretOrKey: 'your_jwt_secret'
+},
     async (jwtPayload, callback) => {
         return await Users.findById(jwtPayload._id)
-            .then((user)=>{
+            .then((user) => {
                 return callback(null, user);
             })
-            .catch((err)=>{
+            .catch((err) => {
                 return callback(err);
             });
     }
--- util.js
+++ util.js
@@ -1,9 +1,17 @@
-// Function used to assess request params validation results with and return true if successful, false if errors present
+//File: /util.js
+
+
+/**
+ * Assess request params validation results.
+ * @param {*} validationErrors 
+ * @param {*} res 
+ * @returns {boolean} `true` if successful validation, `false` if errors present
+ */
 const checkValidationResults = (validationErrors, res) => {
-    if(!validationErrors.isEmpty()){
+    if (!validationErrors.isEmpty()) {
         console.error('Failed Validation - error with request parameter: ');
         console.error(validationErrors);
-        res.status(400).send('Failed Validation - error with request parameter: \n'+JSON.stringify(validationErrors));
+        res.status(400).send('Failed Validation - error with request parameter: \n' + JSON.stringify(validationErrors));
         return false;
     }
     return true;
